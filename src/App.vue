3536c5c9707c11bba74abc621b7e756b71314485
gabrielebottari <gabriele.bottari1@gmail.com>
2024-01-17 13:23:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+    data() {
+        return { count: 0 }
+    },
+    template: `<div>count is {{ count }}</div>`,
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <AppHeader />
+        <h1>
+            Ciao ragazzi
+        </h1>
+        <p>
+            {{ count }}
+        </p>
+        <button @click="count++">
+            incrementa count
+        </button>
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'ciao ragazzi'
+         }
+    },
+    template: `<div>count is {{ count }}</div>`,
+};
+</script>
+
+<template>
+    <div>
+        <header>
+            <h1 class="main-title">
+                {{ title }}
+            </h1>
+        </header>
+    </div>
+
+</template>
+
+<style scoped>
+    header {
+        padding: 20px;
+        background-color: aquamarine;
+    }
+</style>
